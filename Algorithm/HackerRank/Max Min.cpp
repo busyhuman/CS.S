6631ae3346c45bf54c8e97d6f5b84e53517a060b
Hyeondae Kang <busyhuman@naver.com>
2020-01-16 16:59:27 +0900

    Medium

---
+++ Algorithm/HackerRank/Max Min.cpp
@@ -0,0 +1,45 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+// Complete the maxMin function below.
+int maxMin(int k, vector<int> arr) {
+    int len=arr.size(), ans=2e9;
+    sort(arr.begin(),arr.end());
+
+    for(int i=k-1;i<len;i++){
+        ans = min(ans, arr[i]-arr[i-k+1]);
+    }
+    return ans;
+}
+
+int main()
+{
+    ofstream fout(getenv("OUTPUT_PATH"));
+
+    int n;
+    cin >> n;
+    cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
+    int k;
+    cin >> k;
+    cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
+    vector<int> arr(n);
+
+    for (int i = 0; i < n; i++) {
+        int arr_item;
+        cin >> arr_item;
+        cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
+        arr[i] = arr_item;
+    }
+
+    int result = maxMin(k, arr);
+
+    fout << result << "\n";
+
+    fout.close();
+
+    return 0;
+}
